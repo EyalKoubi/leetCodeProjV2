326ffd92cc9779234a9ff879722560658d2226b1
EyalKoubi <109479738+EyalKoubi@users.noreply.github.com>
2025-04-24 10:11:36 +0300

    Update ForgotPasswordModal.js

--- frontend/src/components/ForgotPasswordModal.js
+++ frontend/src/components/ForgotPasswordModal.js
@@ -11,7 +11,7 @@ const ForgotPasswordModal = ({ onClose }) => {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:3002/auth/reset-password", {
+      const res = await fetch("https://leetcodeprojv2.onrender.com/auth/reset-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
